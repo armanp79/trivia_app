c38a58fcaaef6bf8d6456299220b2ae5fe42aa31
carrie <yiyunzhang117@gmail.com>
2020-06-14 20:25:24 -0400

    finish getQuetion api interface

--- backend/app.js
+++ backend/app.js
@@ -12,4 +12,4 @@ app.use(cors());
 app.use(router);
 
 
-app.listen(8080, () => console.log(`Example app listening on port port!`));
+app.listen(process.env.port || 3000, () => console.log(`Example app listening on port port!`));
