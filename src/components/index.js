f29bbf3b88533df3fe2c8f98b36e65b71a70076f
Arman Pithawala <armman103@gmail.com>
2020-06-16 08:55:59 -0700

    basic front end set up

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Choices from './Choices';
+
+const Components = () => {
+    return <>
+    <header className="App-header">
+        <Choices />
+    </header>
+    </>
+}
+
+export default Components;
